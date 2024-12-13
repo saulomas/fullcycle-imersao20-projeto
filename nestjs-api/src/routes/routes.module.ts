e756d62deb0863cf932479ab58a437a0070c9b25
Saulo M. Amaral <saulomas@gmail.com>
2024-12-13 20:24:58 -0300

    Adds Websocket for real-time communication

--- nestjs-api/src/routes/routes.module.ts
+++ nestjs-api/src/routes/routes.module.ts
@@ -3,10 +3,11 @@ import { RoutesService } from './routes.service';
 import { RoutesController } from './routes.controller';
 import { MapsModule } from 'src/maps/maps.module';
 import { RoutesDriverService } from './routes-driver/routes-driver.service';
+import { RoutesDriverGateway } from './routes-driver/routes-driver.gateway';
 
 @Module({
   imports: [MapsModule],
   controllers: [RoutesController],
-  providers: [RoutesService, RoutesDriverService],
+  providers: [RoutesService, RoutesDriverService, RoutesDriverGateway],
 })
 export class RoutesModule {}
--- next-frontend/src/app/admin/page.tsx
+++ next-frontend/src/app/admin/page.tsx
@@ -1,11 +1,48 @@
 "use client";
 
-import { useMap } from '@/hooks/useMap';
-import { useRef } from 'react';
+import { useEffect, useRef } from "react";
+import { useMap } from "../../hooks/useMap";
+import { socket } from "../../utils/socket-io";
 
 export function AdminPage() {
   const mapContainerRef = useRef<HTMLDivElement>(null);
-  useMap(mapContainerRef);
+  const map = useMap(mapContainerRef);
+
+  useEffect(() => {
+    if (!map) {
+      return;
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+    socket.disconnected ? socket.connect() : socket.offAny();
+
+    socket.on(
+      `server:new-points:list`,
+      async (data: { route_id: string; lat: number; lng: number }) => {
+        console.log(data);
+        if (!map.hasRoute(data.route_id)) {
+          const response = await fetch(`${process.env.NEXT_PUBLIC_NEXT_API_URL}/api/routes/${data.route_id}`)
+          const route = await response.json();
+          map.addRouteWithIcons({
+            routeId: data.route_id,
+            startMarkerOptions: {
+              position: route.directions.routes[0].legs[0].start_location
+            },
+            endMarkerOptions: {
+              position: route.directions.routes[0].legs[0].end_location
+            },
+            carMarkerOptions: {
+              position: route.directions.routes[0].legs[0].start_location
+            },
+          });
+        }
+        map.moveCar(data.route_id, { lat: data.lat, lng: data.lng });
+      }
+    );
+    return () => {
+      socket.disconnect();
+    }
+  }, [map]);
 
   return <div className="h-full w-full" ref={mapContainerRef} />;
 }
