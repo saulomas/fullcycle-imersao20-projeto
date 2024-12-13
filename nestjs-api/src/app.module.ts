1987b8d9d45ffa82dc9d7218e493c51bde4100e1
Saulo M. Amaral <saulomas@gmail.com>
2024-12-13 18:37:51 -0300

    Create endpoints for Location search and Routes creation

--- nestjs-api/src/app.module.ts
+++ nestjs-api/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MapsModule } from './maps/maps.module';
+import { ConfigModule } from '@nestjs/config';
+import { RoutesModule } from './routes/routes.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    PrismaModule,
+    MapsModule,
+    RoutesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
